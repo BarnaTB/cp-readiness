05023a3367c56e468d189b2c8ed18f6d1344b522
BarnaTB <barnabastb2@gmail.com>
2018-05-16 19:50:18 +0300

    Modify JS validation file

--- valid.js
+++ valid.js
@@ -1,6 +1,6 @@
 function required(){
-    var valId = document.forms["userInput"]["submt"].value;
-    if (valId == "")
+    var valId = document.userInput.submt.value;
+    if (valId === "")
     {
         alert("Please enter some text.");
         return false;
